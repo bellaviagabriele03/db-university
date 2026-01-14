d15057c58d869c88ce586e0ae3d9a65f9fa32959
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-01-14 16:41:10 +0100

    feat: ex 5 done

--- dbs.sql
+++ dbs.sql
@@ -25,7 +25,9 @@ where period = "I semestre" and year = 1
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
-
+select *
+from exams
+where hour(hour) >= 14 and date = "2020/06/20"
 
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
